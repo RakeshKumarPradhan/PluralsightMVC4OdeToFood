b07339cf5f944c181f9b021d5462f2784bbdc79b
RakeshKumarPradhan <rakeshp108@gmail.com>
2016-09-20 16:57:31 +0530

    pluralsight MVC OdeToFood

---
+++ CuisineController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PluralsightMVC4OdeToFood.Controllers
+{
+
+    public class CuisineController : Controller
+    {
+        //
+        // GET: /Cuisine/
+        public ActionResult Search(string name = "french")
+        {
+
+            //throw new Exception("something terrible has happened"); 
+            var message = Server.HtmlEncode(name);
+            return Content(message) ;
+        }
+
+    }
+}
---
+++ HomeController.cs
@@ -0,0 +1,49 @@
+﻿using PluralsightMVC4OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PluralsightMVC4OdeToFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        OdeToFood _db = new OdeToFood();  
+
+        public ActionResult Index()
+        {
+            var model = _db.Restaurants.ToList(); 
+
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            //ViewBag.Message = "Your app description page.";
+            //ViewBag.Location = "MaryLand , USA"; 
+            var model = new AboutModel();
+            model.Name = "Alan ";
+            model.Location = "Maryland, USA"; 
+
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_db != null)
+            {
+                _db.Dispose(); 
+            }
+            base.Dispose(disposing); 
+        }
+
+    }
+}
